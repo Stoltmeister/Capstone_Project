cd3b76d519ad6c26e953b1e21c19b086943c1935
Joshua Stoltenberg <stoltenberg96@gmail.com>
2018-11-16 16:07:10 -0600

    Working on models

---
+++ CapstoneProject/Models/UserEatery.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapstoneProject.Models
+{
+    public class UserEatery
+    {
+        [Key]
+        public string Id { get; set; }
+        [ForeignKey("StandardUser")]
+        public string StandardUserId { get; set; }
+        public StandardUser StandardUser { get; set; }
+        public string Name { get; set; }
+        public bool IsVegan { get; set; }
+        public bool IsVegetarian { get; set; }
+        public bool HasVeganOptions { get; set; }
+    }
+}
---
+++ CapstoneProject/Models/VeganIngredient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapstoneProject.Models
+{
+    public class VeganIngredient
+    {
+        [Key]
+        public string Id { get; set; }
+        public string OriginalFood { get; set; }
+        public string NewFood { get; set; }
+        public bool IsIngredient { get; set; }
+    }
+}
