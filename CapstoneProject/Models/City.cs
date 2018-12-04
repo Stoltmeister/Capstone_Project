069edb73fce8166c7a131c0470e779257e8b3f61
Joshua Stoltenberg <stoltenberg96@gmail.com>
2018-12-04 16:33:11 -0600

    Form field styling progress

--- CapstoneProject/Models/City.cs
+++ CapstoneProject/Models/City.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace CapstoneProject.Models
 {
     public class City
     {
+        [Display(Name = "City")]
         public string CitySearchKeyword { get; set; }
         public int CityId { get; set; }
     }
